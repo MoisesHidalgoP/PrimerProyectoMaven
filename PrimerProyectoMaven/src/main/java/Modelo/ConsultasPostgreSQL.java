f6c41c39cd4488911b130c16b485f2644ee9dd8d
Moises Hidalgo <csi21-mhidaza@colegioaltair.net>
2022-10-18 22:48:40 +0200

    Commit inicial 18/10/2022

---
+++ PrimerProyectoMaven/src/main/java/Modelo/ConsultasPostgreSQL.java
@@ -0,0 +1,161 @@
+package Modelo;
+
+import java.sql.Connection;
+
+import java.util.Scanner;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import Util.variablesConexionPostgreSQL;
+
+public class ConsultasPostgreSQL {
+
+public static ArrayList<dtoAlumno> selectAllAlumnos(Connection conexionGenerada) {
+		
+	System.out.println("[INFORMACIÓN-consultasPostgreSQL-selectAllAlumnos] Entra en selectAllAlumnos");
+	Statement declaracionSQL = null;
+	ResultSet resultadoConsulta = null;
+	ArrayList<dtoAlumno> listAlumnos = new ArrayList<>();
+		
+		try {
+			
+			//Se abre una declaración
+			declaracionSQL = conexionGenerada.createStatement();
+			//Se define la consulta de la declaración y se ejecuta
+			resultadoConsulta = declaracionSQL.executeQuery("SELECT * FROM \"proyectoEclipse\".\"alumnos\"");
+		    
+			//Llamada a la conversión a dtoAlumno
+			listAlumnos = dtoADto.resultsetAdtoAlumno(resultadoConsulta);
+			int i = listAlumnos.size();
+			System.out.println("[INFORMACIÓN-consultasPostgreSQL-selectAllAlumnos] Número alumnos: "+i);
+			
+			System.out.println("[INFORMACIÓN-consultasPostgreSQL-selectAllAlumnos] Cierre conexión, declaración y resultado");				
+		    resultadoConsulta.close();
+		    declaracionSQL.close();
+		    //conexionGenerada.close();
+			
+		} catch (SQLException e) {
+			
+			System.out.println("[ERROR-conexionPostgresql-main] Error generando la declaracionSQL: " + e);
+			return listAlumnos;
+			
+		}
+		return listAlumnos;
+		
+	}
+
+
+public static ArrayList<dtoAlumnoAsignaturas> selectAllAlumnosAsig(Connection conexionGenerada) {
+	
+	System.out.println("[INFORMACIÓN-consultasPostgreSQL-selectAllAlumnosAsig] Entra en selectAllAlumnosAsig");
+	Statement declaracionSQL = null;
+	ResultSet resultadoConsulta = null;
+	ArrayList<dtoAlumnoAsignaturas> listAlumnosAsig = new ArrayList<>();
+	
+	try {
+		
+		//Se abre una declaración
+		declaracionSQL = conexionGenerada.createStatement();
+		//Se define la consulta de la declaración y se ejecuta
+		resultadoConsulta = declaracionSQL.executeQuery("SELECT al.nombre, al.apellidos,asi.\"nombreAsig\"  FROM \"proyectoEclipse\".alumnos al    INNER JOIN \"proyectoEclipse\".rel_alumn_asig r ON al.\"id_alumno\" = r.id_alumno     INNER JOIN \"proyectoEclipse\".asignatura asi ON r.id_asignatura = asi.id_asignatura");
+	    
+		//Llamada a la conversión a dtoAlumno
+		listAlumnosAsig = dtoADto.resultsetAdtoAlumnoAsig(resultadoConsulta);
+		int i = listAlumnosAsig.size();
+		System.out.println("[INFORMACIÓN-consultasPostgreSQL-selectAllAlumnosAsig] Número alumnos: "+i);
+		
+		System.out.println("[INFORMACIÓN-consultasPostgreSQL-selectAllAlumnosAsig] Cierre conexión, declaración y resultado");				
+	    resultadoConsulta.close();
+	    declaracionSQL.close();
+	    //conexionGenerada.close();
+		
+	} catch (SQLException e) {
+		
+		System.out.println("[ERROR-conexionPostgresql-main] Error generando la declaracionSQL: " + e);
+		return listAlumnosAsig;
+		
+	}
+	return listAlumnosAsig;
+	
+}
+public static void insertNuevoAlumno(String consulta, Connection conexionGenerada) {
+	
+	System.out.println("[INFORMACIÓN-consultasPostgreSQL-insertNuevoAlumno] Entra en insertNuevoAlumno");
+	Statement declaracionSQL = null;
+	
+	
+	
+	
+	try {
+		
+		declaracionSQL = conexionGenerada.createStatement();
+		declaracionSQL.execute(consulta);
+		declaracionSQL.close();
+		
+		System.out.println("[INFORMACIÓN-consultasPostgreSQL-insertNuevoAlumno] Cierre declaración.");
+		
+	} catch (SQLException e) {
+		
+		System.out.println("[ERROR-consultasPostgreSQL-insertNuevoAlumno] Error al insertar alumno: " + e);
+	
+	}
+	
+}
+
+
+public static void deleteAlumnos(String consulta,Connection conexionGenerada) {
+	
+	System.out.println("[INFORMACIÓN-consultasPostgreSQL-deleteAlumnos] Entra en deleteAlumnos");
+	Statement declaracionSQL = null;
+	
+	
+try {
+		
+	declaracionSQL = conexionGenerada.createStatement();
+	declaracionSQL.executeUpdate(consulta);
+	declaracionSQL.close();
+	
+	
+		System.out.println("[INFORMACIÓN-consultasPostgreSQL-deleteAlumno] Cierre declaración.");
+		
+	} catch (SQLException e) {
+		
+		System.out.println("[ERROR-consultasPostgreSQL-deleteAlumno] Error al borrar alumno: " + e);
+	
+	}
+}
+
+public static void updateAlumnos(String consulta,Connection conexionGenerada) {
+	
+	System.out.println("[INFORMACIÓN-consultasPostgreSQL-updateAlumnos] Entra en updateAlumnos");
+	Statement declaracionSQL = null;
+	
+	
+try {
+		
+	declaracionSQL = conexionGenerada.createStatement();
+	declaracionSQL.executeUpdate(consulta);
+	declaracionSQL.close();
+	
+	
+		System.out.println("[INFORMACIÓN-consultasPostgreSQL-updateAlumno] Cierre declaración.");
+		
+	} catch (SQLException e) {
+		
+		System.out.println("[ERROR-consultasPostgreSQL-updateAlumno] Error al borrar alumno: " + e);
+	
+	}
+
+}
+	
+
+	
+}
+
+
+
+
+
+
---
+++ PrimerProyectoMaven/src/main/java/ProyectoMaven/PrimerProyectoMaven/App.java
@@ -0,0 +1,93 @@
+package ProyectoMaven.PrimerProyectoMaven;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Modelo.ConexionPostgreSQL;
+
+import Modelo.ConsultasPostgreSQL;
+import Modelo.dtoAlumno;
+import Modelo.dtoAlumnoAsignaturas;
+import Util.variablesConexionPostgreSQL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//CONSTANTES
+		final String HOST = variablesConexionPostgreSQL.getHost();
+		final String PORT = variablesConexionPostgreSQL.getPort();
+		final String DB = variablesConexionPostgreSQL.getDb();
+		final String USER = variablesConexionPostgreSQL.getUser();
+		final String PASS = variablesConexionPostgreSQL.getPass();
+		ArrayList<dtoAlumno> listAlumnos = new ArrayList<>();
+		ArrayList<dtoAlumnoAsignaturas> listAlumnosAsignaturas = new ArrayList<>();
+
+		
+		/*Se crea una instancia de la clase en la que estamos para poder generar la conexión a PostgreSQL
+		*utilizando el método generaConexion
+		*/
+		System.out.println("[INFORMACIÓN-controladorPortgreSQL-main] Lamada generaConexion");
+		ConexionPostgreSQL conexionPostgresql = new ConexionPostgreSQL();
+		Connection conexionGenerada = conexionPostgresql.generaConexion(HOST,PORT,DB,USER,PASS);
+		
+		if(conexionGenerada != null) {
+			
+			
+			
+			//Llama al método que ejecuta la consulta de insert
+			System.out.println("[INFORMACIÓN-controladorPortgreSQL-main] Llamada insertNuevoAlumno");
+			//Si los métodos se crean como estáticos no es necesario instanciar una clase.
+			ConsultasPostgreSQL.insertNuevoAlumno("INSERT INTO \"proyectoEclipse\".\"alumnos\" (id_alumno,nombre,apellidos,email)"
+					+ "VALUES(100,'Juanjo','Hernandez','jh@altair.es')", conexionGenerada);
+			
+			
+	
+			//Llamar al método que ejecuta la consulta
+			System.out.println("[INFORMACIÓN-controladorPortgreSQL-main] Llamada selectAllAlumnos");
+			listAlumnos = ConsultasPostgreSQL.selectAllAlumnos(conexionGenerada);
+			int i = listAlumnos.size();
+			System.out.println("[INFORMACIÓN-controladorPortgreSQL-main] Número alumnos: "+i);
+			for(Modelo.dtoAlumno alumnos: listAlumnos) {
+				System.out.println(alumnos.getNombre()+ " " +alumnos.getApellidos());			}
+			
+			
+			//Llama al metodo que borra el alumno
+			System.out.println("[INFORMACIÓN-controladorPortgreSQL-main] Llamada deleteAlumnos");
+			// Pedimos un id al usuario para borrar
+		    System.out.print("\n\tIntroduzca el id del alumno para eliminarlo:\t");
+		    Scanner sc = new Scanner(System.in);
+		    int id = sc.nextInt();
+			ConsultasPostgreSQL.deleteAlumnos("DELETE FROM \"proyectoEclipse\".\"alumnos\" WHERE id_alumno = '"+id+"'", conexionGenerada);
+			
+			
+			//Llama al metodo que actualiza el alumno
+			System.out.println("[INFORMACIÓN-controladorPortgreSQL-main] Llamada updateAlumnos");
+			// Pedimos un id al usuario para borrar
+		    System.out.print("\n\tIntroduzca el id del alumno para actualizarlo:\t");
+		    Scanner sc1 = new Scanner(System.in);
+		    int id1 = sc1.nextInt();
+			ConsultasPostgreSQL.deleteAlumnos("UPDATE \"proyectoEclipse\".\"alumnos\" SET nombre = 'Moises', apellidos = 'Hidalgo' WHERE id_alumno = '"+id1+"'", conexionGenerada);
+			
+			
+			}
+		
+		System.out.println("[INFORMACIÓN-controladorPortgreSQL-main] Llamada a cerrarConexion");
+        ConexionPostgreSQL.cerrarConexion(conexionGenerada);
+			
+			
+		}		
+	}
+	
+
+	
+
+
+
+
